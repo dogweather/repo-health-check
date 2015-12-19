75f208cf2477172ab51a3ecd33f8b2ec85808b15
Robb Shecter <robb@weblaws.org>
2015-12-19 11:19:18 -0800

    strict mode and more tests

--- metrics_math.js
+++ metrics_math.js
@@ -1,5 +1,10 @@
 var MetricsMath = {};
 
-MetricsMath.ratio = function(x, y) {
-  return x / y;
-};
+(function() {
+  'use strict';
+
+  MetricsMath.ratio = function(x, y) {
+    return x / y;
+  };
+
+}());
--- test/spec/test.js
+++ test/spec/test.js
@@ -14,6 +14,12 @@
       it('handles a 1:1 ratio', function() {
         expect( MetricsMath.ratio(5, 5) ).toBe(1);
       });
+      it('handles a 1:2 ratio', function() {
+        expect( MetricsMath.ratio(3, 6) ).toBe(0.5);
+      });
+      it('handles a 3:2 ratio', function() {
+        expect( MetricsMath.ratio(9, 6) ).toBe(1.5);
+      });
     });
   });
 
