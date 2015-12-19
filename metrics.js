0a0026559219f5172d62bd786bb0202adc39c3fe
Robb Shecter <robb@weblaws.org>
2015-12-19 12:04:31 -0800

    Metrics.effectiveness_desc()

--- metrics.js
+++ metrics.js
@@ -7,4 +7,17 @@ var Metrics = {};
     return x / y;
   };
 
+  Metrics.effectiveness_desc = function(rating) {
+    if (rating >= 0 && rating <= 3) {
+      return 'in the weeds';
+    }
+    if (rating >= 4 && rating <= 6) {
+      return 'treading water';
+    }
+    if (rating >= 7 && rating <= 10) {
+      return 'on top of it';
+    }
+    throw new RangeError('Rating must be between 0 and 11');
+  };
+
 }());
--- test/spec/test.js
+++ test/spec/test.js
@@ -13,6 +13,18 @@
         expect( Metrics.ratio(9, 6) ).toBe(1.5);
       });
     });
+
+    describe('.effectiveness_desc()', function() {
+      it('handles a range from 0 to 10', function() {
+        expect( Metrics.effectiveness_desc( 0) ).toBe('in the weeds');
+        expect( Metrics.effectiveness_desc( 5) ).toBe('treading water');
+        expect( Metrics.effectiveness_desc(10) ).toBe('on top of it');
+      });
+      it('throws an error when outside 0 to 10', function() {
+        expect( function(){ Metrics.effectiveness_desc(15); } ).toThrowError(RangeError);
+        expect( function(){ Metrics.effectiveness_desc(-1); } ).toThrowError(RangeError);
+      });
+    });
   });
 
 })();
