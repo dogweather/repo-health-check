d267109501ae822a0ccad979be6eae0bb0ba2d90
Robb Shecter <robb@weblaws.org>
2015-12-31 20:35:54 -0800

    first stab at the trend chart

---
+++ src/chart.js
@@ -0,0 +1,23 @@
+google.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Week', 'Effectiveness', 'Activity'],
+    ['12/1', 4.3, 5],
+    ['12/8', 4.9, 6],
+    ['12/15', 6.7, 3],
+    ['12/22', 2.3, 8]
+  ]);
+
+  var options = {
+    title: 'Trends',
+    curveType: 'function',
+    // legend: {
+    //   position: 'bottom'
+    // }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('trend_chart'));
+
+  chart.draw(data, options);
+}
